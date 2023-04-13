8e800fd3d263753da8b6a1a22c6941666c2b63aa
Vitor Hugo Schiphorst <vitorhugo.schiphorst@gmail.com>
2023-04-13 07:59:12 -0300

    feat: controle

---
+++ src/modelo/Medico.java
@@ -0,0 +1,26 @@
+package modelo;
+
+public class Medico {
+	
+	private Integer id;
+	private Integer crm;
+	private String especificação;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getCrm() {
+		return crm;
+	}
+	public void setCrm(Integer crm) {
+		this.crm = crm;
+	}
+	public String getEspecificação() {
+		return especificação;
+	}
+	public void setEspecificação(String especificação) {
+		this.especificação = especificação;
+	}
+}
