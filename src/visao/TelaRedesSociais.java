2b01d64662274705b721886649d11754d21b672e
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-04-24 09:38:06 -0300

    feat: organização da tela redes sociais

--- src/visao/TelaRedesSociais.java
+++ src/visao/TelaRedesSociais.java
@@ -44,7 +44,7 @@ public class TelaRedesSociais extends JFrame {
 		setResizable(false);
 		setTitle("Redes Socias");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 487, 333);
+		setBounds(100, 100, 487, 323);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -52,72 +52,72 @@ public class TelaRedesSociais extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoInsta.png")));
+		lblNewLabel.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/godriImg.png")));
 		lblNewLabel.setBounds(10, 30, 60, 60);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoGithub.png")));
+		lblNewLabel_1.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/vitorImg.png")));
 		lblNewLabel_1.setBounds(10, 120, 60, 60);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("New label");
-		lblNewLabel_1_1.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoLinkedin.png")));
+		lblNewLabel_1_1.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/barbaraImg.png")));
 		lblNewLabel_1_1.setBounds(10, 209, 60, 60);
 		contentPane.add(lblNewLabel_1_1);
 		
 		JLabel lblGitGodri = new JLabel("New label");
 		lblGitGodri.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblGitGodri.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/godriImg.png")));
-		lblGitGodri.setBounds(100, 120, 60, 60);
+		lblGitGodri.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoGithub.png")));
+		lblGitGodri.setBounds(190, 30, 60, 60);
 		contentPane.add(lblGitGodri);
 		
 		JLabel lblGitVitor = new JLabel("New label");
-		lblGitVitor.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/vitorImg.png")));
+		lblGitVitor.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoGithub.png")));
 		lblGitVitor.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		lblGitVitor.setBounds(190, 120, 60, 60);
 		contentPane.add(lblGitVitor);
 		
 		JLabel lblGitBarbara = new JLabel("New label");
 		lblGitBarbara.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblGitBarbara.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/barbaraImg.png")));
-		lblGitBarbara.setBounds(280, 120, 60, 60);
+		lblGitBarbara.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoGithub.png")));
+		lblGitBarbara.setBounds(190, 209, 60, 60);
 		contentPane.add(lblGitBarbara);
 		
 		JLabel lblLinkedinBarbara = new JLabel("New label");
 		lblLinkedinBarbara.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblLinkedinBarbara.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/barbaraImg.png")));
+		lblLinkedinBarbara.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoLinkedin.png")));
 		lblLinkedinBarbara.setBounds(280, 209, 60, 60);
 		contentPane.add(lblLinkedinBarbara);
 		
 		JLabel lblLinkedinGodri = new JLabel("New label");
 		lblLinkedinGodri.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblLinkedinGodri.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/godriImg.png")));
-		lblLinkedinGodri.setBounds(100, 209, 60, 60);
+		lblLinkedinGodri.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoLinkedin.png")));
+		lblLinkedinGodri.setBounds(280, 30, 60, 60);
 		contentPane.add(lblLinkedinGodri);
 		
 		JLabel lblLinkedinVitor = new JLabel("New label");
 		lblLinkedinVitor.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblLinkedinVitor.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/vitorImg.png")));
-		lblLinkedinVitor.setBounds(190, 209, 60, 60);
+		lblLinkedinVitor.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoLinkedin.png")));
+		lblLinkedinVitor.setBounds(280, 120, 60, 60);
 		contentPane.add(lblLinkedinVitor);
 		
 		JLabel lblInstaGodri = new JLabel("New label");
-		lblInstaGodri.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/godriImg.png")));
+		lblInstaGodri.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoInsta.png")));
 		lblInstaGodri.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		lblInstaGodri.setBounds(100, 30, 60, 60);
 		contentPane.add(lblInstaGodri);
 		
 		JLabel lblInstaVitor = new JLabel("New label");
-		lblInstaVitor.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/vitorImg.png")));
+		lblInstaVitor.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoInsta.png")));
 		lblInstaVitor.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblInstaVitor.setBounds(190, 30, 60, 60);
+		lblInstaVitor.setBounds(100, 120, 60, 60);
 		contentPane.add(lblInstaVitor);
 		
 		JLabel lblInstaBarbara = new JLabel("New label");
-		lblInstaBarbara.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/barbaraImg.png")));
+		lblInstaBarbara.setIcon(new ImageIcon(TelaRedesSociais.class.getResource("/img/logoInsta.png")));
 		lblInstaBarbara.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblInstaBarbara.setBounds(280, 30, 60, 60);
+		lblInstaBarbara.setBounds(100, 209, 60, 60);
 		contentPane.add(lblInstaBarbara);
 		
 		MeuBotao mbtSair = new MeuBotao();
