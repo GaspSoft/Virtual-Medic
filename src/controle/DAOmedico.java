df24330908822458d47e2a775256884237ce52ff
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-04-26 10:05:05 -0300

    feat: DAO paciente add UPDATE e DELETE

--- src/controle/DAOmedico.java
+++ src/controle/DAOmedico.java
@@ -28,6 +28,50 @@ public class DAOmedico {
 		return false;
 	}
 	
+	//UPDATE
+		public Boolean alterar(Medico m, String cpf) {
+
+				for(Medico medico : listaMedico) {
+					if (medico.getCpf().equals(cpf) ) {
+						
+						//Classe Pessoa
+						medico.setCpf(m.getCpf());
+						medico.setNome(m.getNome());
+						medico.setEmail(m.getEmail());
+						medico.setGenero(m.getGenero());
+						medico.setIdade(m.getIdade());
+						
+						//Classe Endereço
+						medico.setCep(m.getCep());
+						medico.setUf(m.getUf());
+						medico.setCidade(m.getCidade());
+						medico.setBairro(m.getBairro());
+						medico.setRua(m.getRua());
+						medico.setNumero(m.getNumero());
+						medico.setComplemento(m.getComplemento());
+						
+						//Classe medico
+						medico.setCrm(m.getCrm());
+						medico.setEspecificacao(m.getEspecificacao());
+			
+				return true;
+			}
+		}
+		return false;
+
+		}
+		
+		//DELETE
+		public Boolean deletar(Medico m, String cpf) {
+			for (Medico medico : listaMedico) {
+				if (medico.getCpf().equals(cpf) ){
+					listaMedico.remove(medico);
+					return true;
+				}
+			}
+			return false;
+		}
+	
 	// SELECT ALL
 	public ArrayList<Medico> listaMedico() {
 		return listaMedico;
