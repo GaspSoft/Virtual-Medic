86c46b96f1f2c8b5788c262e29dfa948ead0bc07
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-04-28 17:56:19 -0300

    organixando tabela

--- src/visao/paciente/TelaListaPaciente.java
+++ src/visao/paciente/TelaListaPaciente.java
@@ -2,13 +2,9 @@ package visao.paciente;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-
-
-
 import layoutPersonalizado.componentes.tables.TableActionCellEditor;
 import layoutPersonalizado.componentes.tables.TableActionCellRender;
 import layoutPersonalizado.componentes.tables.TableActionEvent;
@@ -18,9 +14,9 @@ import javax.swing.GroupLayout;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
 
-public class Test extends javax.swing.JFrame {
+public class TelaListaPaciente extends javax.swing.JFrame {
 
-    public Test() {
+    public TelaListaPaciente() {
         initComponents();
         getContentPane().setBackground(Color.WHITE);
         TableCustom.apply(jScrollPane1, TableCustom.TableType.MULTI_LINE);
@@ -44,8 +40,8 @@ public class Test extends javax.swing.JFrame {
                 System.out.println("View row : " + row);
             }
         };
-        jTable1.getColumnModel().getColumn(5).setCellRenderer(new TableActionCellRender());
-        jTable1.getColumnModel().getColumn(5).setCellEditor(new TableActionCellEditor(event));
+        jTable1.getColumnModel().getColumn(3).setCellRenderer(new TableActionCellRender());
+        jTable1.getColumnModel().getColumn(3).setCellEditor(new TableActionCellEditor(event));
         testData(jTable1);
         
     }
@@ -53,52 +49,12 @@ public class Test extends javax.swing.JFrame {
     private void testData(JTable table) {
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         model.addRow(new Object[]{1, "Chai", "Beverages", 18, 39});
-        model.addRow(new Object[]{2, "Chang", "Beverages", 19, 39});
-        model.addRow(new Object[]{3, "Aniseed Syrup", "Beverages", 18, 39});
-        model.addRow(new Object[]{4, "Chef Anton's Cajun Seasoning", "Beverages", 19, 39});
-        model.addRow(new Object[]{5, "Chef Anton's Gumbo Mix", "Beverages", 18, 39});
-        model.addRow(new Object[]{6, "Grandma's Boysenberry Spread", "Beverages", 19, 39});
-        model.addRow(new Object[]{7, "Uncle Bob's Organic Dried Pears", "Beverages", 18, 39});
-        model.addRow(new Object[]{8, "Northwoods Cranberry Sauce", "Beverages", 19, 39});
-        model.addRow(new Object[]{9, "Mishi Kobe Niku", "Beverages", 18, 39});
-        model.addRow(new Object[]{10, "Ikura", "Beverages", 19, 39});
-        model.addRow(new Object[]{11, "Queso Cabrales", "Beverages", 18, 39});
-        model.addRow(new Object[]{12, "Queso Manchego La Pastora", "Beverages", 19, 39});
-        model.addRow(new Object[]{13, "Konbu", "Beverages", 18, 39});
-        model.addRow(new Object[]{14, "Tofu", "Seafood", 19, 39});
-        model.addRow(new Object[]{15, "Genen Shouyu", "Seafood", 18, 39});
-        model.addRow(new Object[]{16, "Pavlova", "Seafood", 19, 39});
-        model.addRow(new Object[]{17, "Alice Mutton", "Seafood", 18, 39});
-        model.addRow(new Object[]{18, "Carnarvon Tigers", "Seafood", 19, 39});
-        model.addRow(new Object[]{19, "Teatime Chocolate Biscuits", "Seafood", 19, 39});
-        model.addRow(new Object[]{20, "Sir Rodney's Marmalade", "Seafood", 19, 39});
-        model.addRow(new Object[]{21, "Sir Rodney's Scones", "Seafood", 19, 39});
-        model.addRow(new Object[]{22, "Gustaf's Knäckebröd", "Seafood", 19, 39});
-        model.addRow(new Object[]{23, "Tunnbröd", "Seafood", 19, 39});
-        model.addRow(new Object[]{24, "Guaraná Fantástica", "Seafood", 19, 39});
-        model.addRow(new Object[]{25, "NuNuCa Nuß-Nougat-Creme", "Seafood", 19, 39});
-        model.addRow(new Object[]{26, "Gumbär Gummibärchen", "Seafood", 19, 39});
-        model.addRow(new Object[]{27, "Schoggi Schokolade", "Seafood", 19, 39});
-        model.addRow(new Object[]{28, "Rössle Sauerkraut", "Seafood", 19, 39});
-        model.addRow(new Object[]{29, "Thüringer Rostbratwurst", "Seafood", 19, 39});
-        model.addRow(new Object[]{30, "Nord-Ost Matjeshering", "Seafood", 19, 39});
-        model.addRow(new Object[]{31, "Gorgonzola Telino", "Seafood", 19, 39});
-        model.addRow(new Object[]{32, "Mascarpone Fabioli", "Seafood", 19, 39});
-        model.addRow(new Object[]{33, "Geitost", "Seafood", 19, 39});
-        model.addRow(new Object[]{34, "Sasquatch Ale", "Seafood", 19, 39});
-        model.addRow(new Object[]{35, "Steeleye Stout", "Seafood", 19, 39});
-        model.addRow(new Object[]{36, "Inlagd Sill", "Seafood", 19, 39});
-        model.addRow(new Object[]{37, "Gravad lax", "Seafood", 19, 39});
-        model.addRow(new Object[]{38, "Côte de Blaye", "Seafood", 19, 39});
-        model.addRow(new Object[]{39, "Chartreuse verte", "Seafood", 19, 39});
-        model.addRow(new Object[]{40, "Boston Crab Meat", "Seafood", 19, 39});
-        model.addRow(new Object[]{41, "Jack's New England Clam Chowder", "Seafood", 19, 39});
-        model.addRow(new Object[]{42, "Singaporean Hokkien Fried Mee", "Seafood", 19, 39});
-        model.addRow(new Object[]{43, "Ipoh Coffee", "Seafood", 19, 39});
+        model.addRow(new Object[]{1, "Chai", "Beverages", 18, 39});
+        model.addRow(new Object[]{1, "Chai", "Beverages", 18, 39});
+        model.addRow(new Object[]{1, "Chai", "Beverages", 18, 39});
     }
 
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+
     private void initComponents() {
 
         tableScrollButton1 = new layoutPersonalizado.componentes.tables.TableScrollButton();
@@ -113,17 +69,18 @@ public class Test extends javax.swing.JFrame {
 
             },
             new String [] {
-                "ID", "Name", "Category", "Price", "Stock", "Action"
+                "CPF", "Nome", "Email", "Ações"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, true
+                false, false, false, true
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
+        
         jScrollPane1.setViewportView(jTable1);
         if (jTable1.getColumnModel().getColumnCount() > 0) {
             jTable1.getColumnModel().getColumn(0).setPreferredWidth(50);
@@ -162,18 +119,18 @@ public class Test extends javax.swing.JFrame {
 
         pack();
         setLocationRelativeTo(null);
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
         jTable1.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }
 
     public static void main(String args[]) {
         
     	EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Test frame = new Test();
+					TelaListaPaciente frame = new TelaListaPaciente();
 					frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -185,7 +142,6 @@ public class Test extends javax.swing.JFrame {
 
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
