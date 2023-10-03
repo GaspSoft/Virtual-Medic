373c574493c80015bddfa7f0eedc285b1485376e
PedroGodri <pedro.g2006@aluno.ifsc.edu.br>
2023-10-03 08:53:35 -0300

    DAOTEST

--- Virtual-Medic-2.0/src/test/java/teste/DAOplanoSaudeTest.java
+++ Virtual-Medic-2.0/src/test/java/teste/DAOplanoSaudeTest.java
@@ -1,5 +1,22 @@
 package teste;
 
-public class DAOplanoSaudeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import controle.DAOplanoSaude;
+import modelo.PlanoSaude;
+
+public class DAOplanoSaudeTest {
+	
+	@Test 
+	public void inserirPlanoSaudeTest() {
+		PlanoSaude pl = new PlanoSaude();
+		
+		pl.setId(2);
+		pl.setNome("Unimed - PR");
+		DAOplanoSaude dao = new DAOplanoSaude();
+		boolean plInserido = dao.inserir(pl);
+		assertEquals(true, plInserido);
+	}
 }
